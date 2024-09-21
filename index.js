0347d3b4d01c5e8b3c4ec101c9cee80b6e5807c6
Asmaa MOHAMEDI <asmaamohamedi.dev@gmail.com>
2024-09-21 18:03:26 +0100

    asmaa

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const modulo = require("./modulo");
 const minus = require("./minus")
 const plus = require("./plus");
 
-console.log(power(1,2));
+console.log(power(3,2));
 console.log(modulo(9,2));
 console.log(minus(30,7));
 console.log(plus(4,4));
